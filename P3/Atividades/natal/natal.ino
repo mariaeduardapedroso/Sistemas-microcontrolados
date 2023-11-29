a7ec322706c3f0b331dfead296f54444f04ae2e4
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2023-11-29 10:06:46 -0300

    Update natal.ino

--- P3/Atividades/natal/natal.ino
+++ P3/Atividades/natal/natal.ino
@@ -1,6 +1,10 @@
 #include <Keypad.h>
 #include <LiquidCrystal.h>
 #include <Servo.h>
+
+#include <SPI.h>
+#include <MFRC522.h>
+
 #define NOTE_B0 31
 #define NOTE_C1 33
 #define NOTE_CS1 35
@@ -91,6 +95,9 @@
 #define NOTE_D8 4699
 #define NOTE_DS8 4978
 
+#define RST_PIN         7          // Configurable, see typical pin layout above
+#define SS_PIN          53         // Configurable, see typical pin layout above
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 Servo myservo;
 const int redled = 10;
@@ -113,11 +120,20 @@ byte colPins[cols] = {5, 6, 7};
 Keypad keypad = Keypad(makeKeymap(key), rowPins, colPins, rows, cols);
 
 void setup() {
+  Serial.begin(9600);   // Initialize serial communications with the PC
+  
   lcd.begin(16, 2);
   pinMode(redled, OUTPUT);
   pinMode(greenled, OUTPUT);
   pinMode(buzz, OUTPUT);
   myservo.attach(9); // SERVO ATTACHED
+
+  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  delay(4);       // Optional delay. Some board do need more time after init to be ready, see Readme
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
 void loop() {
@@ -137,6 +153,18 @@ void loop() {
 
   // Example: Rotating the servo motor
 //  rotateServo();
+// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
 
 void playChristmasTune() {
@@ -159,6 +187,11 @@ void rotateServo() {
   myservo.write(0);   // Return to initial position
   delay(2000);        // Wait for 2 seconds
 }
+
+
+
+
+
 //Programa: Teste de Display LCD 16 x 2
 //Autor: MakerHero
  
@@ -200,3 +233,4 @@ void rotateServo() {
 //    delay(300);
 //  }
 //}
+

25c2430bdd304e278d1d864a3612e4464953313b
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2023-12-02 19:52:12 -0300

    Update natal.ino

--- P3/Atividades/natal/natal.ino
+++ P3/Atividades/natal/natal.ino
@@ -1,10 +1,3 @@
-#include <Keypad.h>
-#include <LiquidCrystal.h>
-#include <Servo.h>
-
-#include <SPI.h>
-#include <MFRC522.h>
-
 #define NOTE_B0 31
 #define NOTE_C1 33
 #define NOTE_CS1 35
@@ -95,38 +88,80 @@
 #define NOTE_D8 4699
 #define NOTE_DS8 4978
 
+#include <Keypad.h>
+#include <LiquidCrystal.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+//rfid
 #define RST_PIN         7          // Configurable, see typical pin layout above
 #define SS_PIN          53         // Configurable, see typical pin layout above
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
+//magnetica tranca
+const int SENSOR_PIN = 13;
+
+const int DARK_THRESHOLD = 100; // you might need to adjust this based on testing
+
+//servomotor
 Servo myservo;
 const int redled = 10;
 const int greenled = 11;
 const int buzz = 8;
+int pos = 0; // LCD Connections
+bool cofreAberto = false;
 
+//lcd
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 const byte rows = 4;
 const byte cols = 3;
 
-char key[rows][cols] = {
-  {'1', '2', '3'},
-  {'4', '5', '6'},
-  {'7', '8', '9'},
-  {'*', '0', '#'}
+////teclado
+const byte qtdLinhas = 4; //QUANTIDADE DE LINHAS DO TECLADO
+const byte qtdColunas = 4; //QUANTIDADE DE COLUNAS DO TECLADO
+
+//CONSTRUÇÃO DA MATRIZ DE CARACTERES
+char matriz_teclas[qtdLinhas][qtdColunas] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 };
+byte PinosqtdLinhas[qtdLinhas] = {38, 39, 40, 41}; //PINOS UTILIZADOS PELAS LINHAS
+byte PinosqtdColunas[qtdColunas] = {42, 43, 44, 45}; //PINOS UTILIZADOS PELAS COLUNAS
+//INICIALIZAÇÃO DO TECLADO
+Keypad meuteclado = Keypad( makeKeymap(matriz_teclas), PinosqtdLinhas, PinosqtdColunas, qtdLinhas, qtdColunas);
 
-byte rowPins[rows] = {1, 2, 3, 4};
-byte colPins[cols] = {5, 6, 7};
-Keypad keypad = Keypad(makeKeymap(key), rowPins, colPins, rows, cols);
+//aleatorio
+int tamanhoSenha = 4;
+char password[10] = "4567";
+char passwordEscrito[10];
+int currentposition = 0;
+int invalidcount = 0;
+bool portaAberta = false;
 
-void setup() {
-  Serial.begin(9600);   // Initialize serial communications with the PC
-  
-  lcd.begin(16, 2);
+// Definição das funções
+void displayscreen();
+void keypress();
+void incorrect();
+void torture1();
+void torture2();
+void counterbeep();
+void unlockbuzz();
+
+void setup()
+{
+  displayscreen();
+  Serial.begin(9600);
   pinMode(redled, OUTPUT);
   pinMode(greenled, OUTPUT);
   pinMode(buzz, OUTPUT);
-  myservo.attach(9); // SERVO ATTACHED
+  myservo.attach(9); //SERVO ATTACHED//
+
+  pinMode(SENSOR_PIN, INPUT_PULLUP);
+
+  lcd.begin(16, 2);
 
   while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
   SPI.begin();      // Init SPI bus
@@ -134,26 +169,95 @@ void setup() {
   delay(4);       // Optional delay. Some board do need more time after init to be ready, see Readme
   mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+
+  inicialTune();
 }
 
-void loop() {
-  // Your main code logic goes here
-  // ...
- myservo.write(90);  // Rotate 90 degrees
-  delay(2000);        // Wait for 2 seconds
-  myservo.write(0);   // Return to initial position
-  delay(2000);        // Wait for 2 seconds
-  // Example: Playing a Christmas tune
-  playChristmasTune();
+void loop()
+{
+  if (portaAberta) {
+    int proximidade = digitalRead(SENSOR_PIN);
+    if (proximidade == LOW) {
+      Serial.println("FECHADO 2");
+      counterbeep();
+      inicialTune();
+    }
+  } else {
+    char tecla_pressionada = meuteclado.getKey(); //VERIFICA SE ALGUMA DAS TECLAS FOI PRESSIONADA
 
-  // Example: Displaying "Feliz Natal" on LCD
-  lcd.clear();
-  lcd.setCursor(1, 0);
-  lcd.print("Feliz Natal");
+    if (tecla_pressionada) { //SE ALGUMA TECLA FOR PRESSIONADA, FAZ
+      Serial.print("Tecla pressionada : "); //IMPRIME O TEXTO NO MONITOR SERIAL
+      Serial.println(tecla_pressionada); //IMPRIME NO MONITOR SERIAL A TECLA PRESSIONADA
+      passwordEscrito[currentposition] = tecla_pressionada;
+    }
 
-  // Example: Rotating the servo motor
-//  rotateServo();
-// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+
+    if ( currentposition == 0)
+    {
+      displayscreen();
+    }
+
+    int l ;
+
+    char code = tecla_pressionada;
+    if (code != NO_KEY)
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("SENHA:");
+      lcd.setCursor(3, 1);
+      for (l = 0; l <= currentposition; ++l)
+      {
+
+        lcd.print("*");
+        keypress();
+      }
+      currentposition = currentposition + 1;
+
+      if (currentposition == tamanhoSenha) {
+        bool verificaSenha = true;
+        currentposition = 0;
+        for (l = 0; l <= tamanhoSenha; ++l) {
+          if (passwordEscrito[l] != password[l]) {
+            //erro
+            Serial.print("ERRO SENHA: ");
+            Serial.print(passwordEscrito[l]);
+            Serial.print(" != ");
+            Serial.println(password[l]);
+            verificaSenha = false;
+          }
+        }
+
+        if (verificaSenha) {
+          unlockdoor();
+          Serial.println("INVALIDO CONTADOR OK");
+          Serial.println(invalidcount);
+          invalidcount = 0;
+        } else {
+          invalidcount = invalidcount + 1;
+          Serial.println("INVALIDO CONTADOR BAD");
+          Serial.println(invalidcount);
+          incorrect();
+        }
+
+      }
+
+      if (invalidcount == 4)
+      {
+        ++invalidcount;
+        torture1();
+      }
+      if (invalidcount >= 8)
+      {
+        torture2();
+      }
+    }
+
+
+  }
+
+
+  //RFID
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
@@ -165,9 +269,10 @@ void loop() {
 
   // Dump debug info about the card; PICC_HaltA() is automatically called
   mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  // LOOP ENDS!!!//
 }
 
-void playChristmasTune() {
+void inicialTune() {
   // Replace this with the notes and durations of your Christmas tune
   int melody[] = {NOTE_E5, NOTE_E5, NOTE_E5, NOTE_B4, NOTE_D5, NOTE_C5, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_E5, NOTE_D5, NOTE_D5};
   int noteDurations[] = {8, 8, 4, 8, 8, 4, 8, 8, 8, 8, 8, 4};
@@ -181,56 +286,195 @@ void playChristmasTune() {
   }
 }
 
-void rotateServo() {
-  myservo.write(90);  // Rotate 90 degrees
-  delay(2000);        // Wait for 2 seconds
-  myservo.write(0);   // Return to initial position
-  delay(2000);        // Wait for 2 seconds
+//*********************Abrir a porta****************************//
+
+void unlockdoor()
+{
+  delay(900);
+
+  lcd.setCursor(0, 0);
+  lcd.println(" ");
+  lcd.setCursor(1, 0);
+  lcd.print("Autorizado");
+  lcd.setCursor(4, 1);
+  lcd.print("BEM-VINDO");
+
+  unlockbuzz();
+
+  for (pos = 180; pos >= 0; pos -= 5) //   De 180 graus para 0 graus
+  {
+    myservo.write(pos); // servo para posição na variável 'pos'
+    delay(5); // espera 15ms para servo atingir a posição
+  }
+  delay(2000);
+
+  portaAberta = true;
+
 }
 
 
+//*****************Código errado***********************//
 
+void incorrect()
+{
+  delay(500);
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("CODIGO");
+  lcd.setCursor(1, 1);
+  lcd.print("INCORRETO");
+  lcd.setCursor(15, 1);
 
+  Serial.println("CÓDIGO INCORRETO. NÃO AUTORIZADO");
+  digitalWrite(redled, HIGH);
+  tone(buzz, NOTE_E1, 1000);  // Você pode ajustar a frequência e a duração do beep aqui
+  delay(500);  // Adiciona um pequeno atraso para separar beeps consecutivos (ajuste conforme necessário)
+  noTone(buzz);  // Para o beep
+  delay(3000);
+  lcd.clear();
+  digitalWrite(redled, LOW);
+  displayscreen();
+}
+//*************Limpar tela****************//
+void clearscreen()
+{
+  lcd.setCursor(0, 0);
+  lcd.println(" ");
+  lcd.setCursor(0, 1);
+  lcd.println(" ");
+  lcd.setCursor(0, 2);
+  lcd.println(" ");
+  lcd.setCursor(0, 3);
+  lcd.println(" ");
+}
+//******************************KEYPRESS***********************************//
+void keypress()
+{
+  tone(buzz, NOTE_C6, 100);  // Você pode ajustar a frequência e a duração do beep aqui
+  delay(50);  // Adiciona um pequeno atraso para separar beeps consecutivos (ajuste conforme necessário)
+  noTone(buzz);  // Para o beep
+}
+//************************Display****************************//
+void displayscreen()
+{
+  lcd.setCursor(0, 0);
+  lcd.println("*DIGITE A SENHA*");
+}
+//*************************** ARM SERVO****************************************************************************//
+void armservo()
+{
+  for (pos = 180; pos <= 180; pos += 50)
+  {
+    myservo.write(pos);
+    delay(5);
+  }
+  delay(5000);
 
-//Programa: Teste de Display LCD 16 x 2
-//Autor: MakerHero
- 
-//Carrega a biblioteca LiquidCrystal
-//#include <LiquidCrystal.h>
-// 
-////Define os pinos que serão utilizados para ligação ao display
-//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-// 
-//void setup()
-//{
-//  //Define o número de colunas e linhas do LCD
-//  lcd.begin(16, 2);
-//}
-// 
-//void loop()
-//{
-//  //Limpa a tela
-//  lcd.clear();
-//  //Posiciona o cursor na coluna 3, linha 0;
-//  lcd.setCursor(3, 0);
-//  //Envia o texto entre aspas para o LCD
-//  lcd.print("MakerHero");
-//  lcd.setCursor(3, 1);
-//  lcd.print(" LCD 16x2");
-//  delay(5000);
-//   
-//  //Rolagem para a esquerda
-//  for (int posicao = 0; posicao < 3; posicao++)
-//  {
-//    lcd.scrollDisplayLeft();
-//    delay(300);
-//  }
-//   
-//  //Rolagem para a direita
-//  for (int posicao = 0; posicao < 6; posicao++)
-//  {
-//    lcd.scrollDisplayRight();
-//    delay(300);
-//  }
-//}
+  for (pos = 180; pos >= 0; pos -= 50)
+  {
+    myservo.write(pos);
+  }
+}
+//********************************UNLOCK BUZZ***********************************//
+void unlockbuzz()
+{
 
+  for (int i = 0; i < 4; i++) {
+    tone(buzz, NOTE_C6, 100);  // Você pode ajustar a frequência e a duração do beep aqui
+    delay(500);
+    noTone(buzz);
+  }
+}
+
+//*******************************COUNTER BEEP*************************************//
+void counterbeep()
+{
+  delay(1200);
+
+  lcd.clear();
+
+  lcd.setCursor(2, 15);
+  lcd.println(" ");
+  lcd.setCursor(2, 14);
+  lcd.println(" ");
+  lcd.setCursor(2, 0);
+  delay(200);
+  lcd.print("FECHANDO EM:");
+  for (int i = 5; i > 0; i--) {
+    lcd.setCursor(3, 1);
+    lcd.print(i);
+    keypress();
+    delay(1000);
+  }
+
+  for (pos = 0; pos <= 180; pos += 5) // de 0 graus para 180 graus
+  { // in steps of 1 degree
+    myservo.write(pos); // servo para posição na variável 'pos'
+  }
+  delay(15);
+
+  currentposition = 0;
+
+  lcd.clear();
+  displayscreen();
+
+  lcd.clear();
+  lcd.setCursor(4, 0);
+  lcd.print("FECHADO!");
+  delay(440);
+
+  portaAberta = false;
+}
+//*****************************TORTURE1****************************************//
+void torture1()
+{
+  //  delay(1000);
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("AGUARDE POR ");
+  lcd.setCursor(5, 1);
+  lcd.print("15 SEGUNDOS.");
+  digitalWrite(buzz, HIGH);
+  delay(15000);
+  digitalWrite(buzz, LOW);
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("POR FAVOR..");
+  lcd.setCursor(1, 1);
+  lcd.print("TENTE NOVAMENTE");
+  delay(3500);
+  lcd.clear();
+
+}
+//******************************TORTURE2*****************************************//
+void torture2()
+{
+  //  delay(1000);
+  lcd.setCursor(1, 0);
+  lcd.print(" ");
+  lcd.setCursor(2, 0);
+  lcd.print("MUITAS");
+  lcd.setCursor(0, 1);
+  lcd.print(" TENTATIVAS!! ");
+  delay(1500);
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("AGUARDE POR ");
+  lcd.setCursor(4, 1);
+  lcd.print(" 1 MINUTO");
+  digitalWrite(buzz, HIGH);
+  delay(55000);
+  
+  lcd.clear();
+  digitalWrite(buzz, LOW);
+  lcd.setCursor(2, 0);
+  lcd.print("TENTE ACERTAR");
+  lcd.setCursor(1, 1);
+  lcd.print("TA DIFICIL??");
+  delay(2500);
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("Ha Ha Ha Ha");
+  delay(1700);
+  lcd.clear();
+}

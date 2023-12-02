5caa75a6a99a537335b014538dccf0ef55aac873
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2023-12-02 15:44:38 -0300

    Update projeto_final.ino

--- P3/Atividades/projeto_final/projeto_final.ino
+++ P3/Atividades/projeto_final/projeto_final.ino
@@ -88,7 +88,7 @@
 #define NOTE_D8 4699
 #define NOTE_DS8 4978
 
-//#include <Keypad.h>
+#include <Keypad.h>
 #include <LiquidCrystal.h>
 #include <Servo.h>
 #include <SPI.h>
@@ -99,6 +99,8 @@
 #define SS_PIN          53         // Configurable, see typical pin layout above
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
+//magnetica tranca
+const int SENSOR_PIN = 13;
 
 const int DARK_THRESHOLD = 100; // you might need to adjust this based on testing
 
@@ -115,23 +117,26 @@ const byte rows = 4;
 const byte cols = 3;
 
 ////teclado
-//char key[rows][cols] = {
-//  {'1', '2', '3'},
-//  {'4', '5', '6'},
-//  {'7', '8', '9'},
-//  {'*', '0', '#'}
-//};
-//byte rowPins[rows] = {21, 22, 23, 24};
-//byte colPins[cols] = {25, 26, 27};
-//Keypad keypad = Keypad(makeKeymap(key), rowPins, colPins, rows, cols);
-// Define os pinos para os botões
-const int buttonPins[] = {22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33};
-const int numButtons = 12; // Número total de botões
+const byte qtdLinhas = 4; //QUANTIDADE DE LINHAS DO TECLADO
+const byte qtdColunas = 4; //QUANTIDADE DE COLUNAS DO TECLADO
+
+//CONSTRUÇÃO DA MATRIZ DE CARACTERES
+char matriz_teclas[qtdLinhas][qtdColunas] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+byte PinosqtdLinhas[qtdLinhas] = {38, 39, 40, 41}; //PINOS UTILIZADOS PELAS LINHAS
+byte PinosqtdColunas[qtdColunas] = {42, 43, 44, 45}; //PINOS UTILIZADOS PELAS COLUNAS
+//INICIALIZAÇÃO DO TECLADO
+Keypad meuteclado = Keypad( makeKeymap(matriz_teclas), PinosqtdLinhas, PinosqtdColunas, qtdLinhas, qtdColunas);
 
 //aleatorio
+int tamanhoSenha = 4;
 char password[10] = "4567";
 int currentposition = 0;
-int invalidcount = 12;
+int invalidcount = 0;
 
 
 
@@ -144,6 +149,8 @@ void setup()
   pinMode(buzz, OUTPUT);
   myservo.attach(9); //SERVO ATTACHED//
 
+  pinMode(SENSOR_PIN, INPUT_PULLUP);
+
   lcd.begin(16, 2);
 
   while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
@@ -152,10 +159,6 @@ void setup()
   delay(4);       // Optional delay. Some board do need more time after init to be ready, see Readme
   mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
-
-  for (int i = 0; i < numButtons; i++) {
-    pinMode(buttonPins[i], INPUT_PULLUP);
-  }
 
   inicialTune();
 }
@@ -176,133 +179,91 @@ void inicialTune() {
 
 void loop()
 {
+  char tecla_pressionada = meuteclado.getKey(); //VERIFICA SE ALGUMA DAS TECLAS FOI PRESSIONADA
+
+  if (tecla_pressionada) { //SE ALGUMA TECLA FOR PRESSIONADA, FAZ
+    Serial.print("Tecla pressionada : "); //IMPRIME O TEXTO NO MONITOR SERIAL
+    Serial.println(tecla_pressionada); //IMPRIME NO MONITOR SERIAL A TECLA PRESSIONADA
+  }
+
+
   if ( currentposition == 0)
   {
     displayscreen();
 
   }
+
   int l ;
-  //  mudar
-  //  char code = keypad.getKey();
-  //  if(qualquer botão pressionado){
-  //    code = valor;
-  //  }
-  for (int i = 0; i < numButtons; i++) {
-    if (digitalRead(buttonPins[i]) == LOW) {
-      delay(50); // Debounce
 
-      while (digitalRead(buttonPins[i]) == LOW) {} // Aguarda o botão ser solto
+  char code = tecla_pressionada;
+  if (code != NO_KEY)
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("SENHA:");
+    lcd.setCursor(7, 1);
+    lcd.print(" ");
+    lcd.setCursor(7, 1);
+    for (l = 0; l <= currentposition; ++l)
+    {
 
-      // Imprime o número correspondente ao botão pressionado no Monitor Serial
-      Serial.println(i + 1); // Adiciona +1 para começar do número 1 ao invés do índice 0
-      // Aqui você pode adicionar a lógica para lidar com cada botão pressionado
-      char code;
+      lcd.print("*");
+      keypress();
+    }
 
-      switch (i) {
-        case 0:
-          code = '1';
-          break;
-        case 1:
-          code = '2';
-          break;
-
-        case 2:
-          code = '3';
-          break;
-
-        case 3:
-          code = '4';
-          break;
-
-        case 4:
-          code = '5';
-          break;
-
-        case 5:
-          code = '6';
-          break;
-
-        case 6:
-          code = '7';
-          break;
-
-        case 7:
-          code = '8';
-          break;
-
-        case 8:
-          code = '9';
-          break;
-
-        case 9:
-          code = '0';
-          break;
-
-        case 10:
-          code = '#';
-          break;
-
-        case 11:
-          code = '*';
-          break;
-
-      }
-
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("SENHA:");
-      lcd.setCursor(7, 1);
-      lcd.print(" ");
-      lcd.setCursor(7, 1);
-      for (l = 0; l <= currentposition; ++l)
+    if (code == password[currentposition])
+    {
+      ++currentposition;
+      if (currentposition == tamanhoSenha)
       {
 
-        lcd.print("*");
-        keypress();
-      }
-      Serial.print("Currentposition : ");
-      Serial.println(password[currentposition]); // Adiciona +1 para começar do número 1 ao invés do índice 0
-      if (code == password[currentposition])
-      {
-        ++currentposition;
-        if (currentposition == 4)
-        {
-
-
-
-          unlockdoor();
-          currentposition = 0;
-
-        }
-
-      }
-
-      else
-      {
-        ++invalidcount;
-        incorrect();
+        unlockdoor();
         currentposition = 0;
 
       }
-      if (invalidcount == 5)
-      {
+      Serial.println("INVALIDO CONTADOR OK");
+      invalidcount = 0;
 
-        ++invalidcount;
-        torture1();
-
-      }
-      if (invalidcount == 8)
-      {
-        torture2();
-      }
     }
+    else
+    {
+      invalidcount = invalidcount + 1;
+      Serial.println("INVALIDO CONTADOR BAD");
+      Serial.println(invalidcount);
+      incorrect();
+      currentposition = 0;
+
+    }
+    if (invalidcount == 5)
+    {
+
+      ++invalidcount;
+      torture1();
+
+    }
+    if (invalidcount >= 8)
+    {
+      torture2();
+    }
+
   }
-  //  if (code != NO_KEY)
-  //  {
-  //
-  //
-  //  }
-  //
+
+
+
+
+
+  int proximidade = digitalRead(SENSOR_PIN);
+  if (proximidade == LOW) {
+    Serial.println("FECHADO");
+    inicialTune();
+  }
+
+
+
+
+
+
+
   //RFID
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
@@ -375,11 +336,12 @@ void incorrect()
 
   Serial.println("CÓDIGO INCORRETO. NÃO AUTORIZADO");
   digitalWrite(redled, HIGH);
-  digitalWrite(buzz, HIGH);
+  tone(buzz, NOTE_E1, 1000);  // Você pode ajustar a frequência e a duração do beep aqui
+  delay(500);  // Adiciona um pequeno atraso para separar beeps consecutivos (ajuste conforme necessário)
+  noTone(buzz);  // Para o beep
   delay(3000);
   lcd.clear();
   digitalWrite(redled, LOW);
-  digitalWrite(buzz, LOW);
   displayscreen();
 }
 //*************Limpar tela****************//
@@ -397,12 +359,9 @@ void clearscreen()
 //******************************KEYPRESS***********************************//
 void keypress()
 {
-
-
-
-  digitalWrite(buzz, HIGH);
-  delay(50);
-  digitalWrite(buzz, LOW);
+  tone(buzz, NOTE_C6, 100);  // Você pode ajustar a frequência e a duração do beep aqui
+  delay(50);  // Adiciona um pequeno atraso para separar beeps consecutivos (ajuste conforme necessário)
+  noTone(buzz);  // Para o beep
 }
 //************************Display****************************//
 void displayscreen()
@@ -432,22 +391,11 @@ void armservo()
 void unlockbuzz()
 {
 
-  digitalWrite(buzz, HIGH);
-  delay(80);
-  digitalWrite(buzz, LOW);
-  delay(80);
-  digitalWrite(buzz, HIGH);
-  delay(80);
-  digitalWrite(buzz, LOW);
-  delay(200);
-  digitalWrite(buzz, HIGH);
-  delay(80);
-  digitalWrite(buzz, LOW);
-  delay(80);
-  digitalWrite(buzz, HIGH);
-  delay(80);
-  digitalWrite(buzz, LOW);
-  delay(80);
+  for (int i = 0; i < 4; i++) {
+    tone(buzz, NOTE_C6, 100);  // Você pode ajustar a frequência e a duração do beep aqui
+    delay(500);
+    noTone(buzz);
+  }
 }
 
 //*******************************COUNTER BEEP*************************************//
@@ -472,19 +420,19 @@ void counterbeep()
   delay(200);
   lcd.clear();
   lcd.setCursor(2, 0);
-  lcd.print("FECHANDO EM:::");
+  lcd.print("FECHANDO EM:");
   digitalWrite(buzz, LOW);
   delay(1000);
   //2
   digitalWrite(buzz, HIGH);
   lcd.setCursor(2, 0);
-  lcd.print("FECHANDO EM:::");
+  lcd.print("FECHANDO EM:");
   lcd.setCursor(4, 1); //2
   lcd.print("4");
   delay(100);
   lcd.clear();
   lcd.setCursor(2, 0);
-  lcd.print("FECHANDO EM:::");
+  lcd.print("FECHANDO EM:");
   digitalWrite(buzz, LOW);
   delay(1000);
   //3
@@ -561,17 +509,17 @@ void torture1()
   delay(1000);
   lcd.clear();
   lcd.setCursor(2, 0);
-  lcd.print("WAIT FOR ");
+  lcd.print("AGUARDE POR ");
   lcd.setCursor(5, 1);
-  lcd.print("15 SECONDS");
+  lcd.print("15 SEGUNDOS.");
   digitalWrite(buzz, HIGH);
   delay(15000);
   digitalWrite(buzz, LOW);
   lcd.clear();
   lcd.setCursor(2, 0);
-  lcd.print("LOL..");
+  lcd.print("POR FAVOR..");
   lcd.setCursor(1, 1);
-  lcd.print(" HOW WAS THAT??");
+  lcd.print("TENTE NOVAMENTE");
   delay(3500);
   lcd.clear();
 
